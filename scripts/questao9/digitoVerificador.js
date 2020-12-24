af74e02cde5eae24a5a06502fe4a5bb079d6740c
NycolasR <nycolasramon3@gmail.com>
2020-12-24 03:37:11 -0300

    9º questão resolvida

---
+++ scripts/questao9/digitoVerificador.js
@@ -0,0 +1,23 @@
+console.log(digitoVerificador("55555"));
+
+/**
+* @prop {string} clientId:
+* @return {number} The check digit.
+*/
+function digitoVerificador(clientId) {
+    // Enquanto a string do clientId não for igual a 1 (um dígito)
+    while(clientId.length > 1) {
+        // Digito de verificação deve ser reinicializado sempre que clientId tiver mais de um dígito
+        var digitoVerificacao = 0;
+
+        // Cada dígito em clientId será convertido em número e somado ao dígito de verificação
+        for (let i = 0; i < clientId.length; i++) {
+            digitoVerificacao += Number(clientId[i]);
+        }
+
+        // A próxima string com dígitos a serem somados
+        clientId = String(digitoVerificacao);
+    }
+
+    return digitoVerificacao;
+}
